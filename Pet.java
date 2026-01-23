31dd43ab73731cfcb69fc2e4365478f5ba3dd432
aibak-cheema <cheemaa@whitman.edu>
2026-01-23 15:22:24 -0800

    aibak.java

--- Pet.java
+++ Pet.java
@@ -14,6 +14,7 @@ public class Pet {
         return name;
     }
 
+    
     // Getter for species
     public String getSpecies() {
         return species;
@@ -24,6 +25,7 @@ public class Pet {
         // Create a Pet object
         Pet pet1= new Pet("Buddy", "Dog");
         Pet maxPet = new Pet("Gary", "Snail");
+        Pet pet2= new Pet("Bruno", "Dog");
 
         System.out.println("my pets name is + " + maxPet.getName());
    
