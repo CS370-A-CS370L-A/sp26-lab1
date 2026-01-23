facc3b53039fbf062df6bcdf05bd9e8e98286319
tandiaa005 <tandiaa@whitman.edu>
2026-01-23 15:49:07 -0800

    Fix syntax errors and add AhmedPet instance
    Fix syntax errors in Pet.java and add AhmedPet.

--- Pet.java
+++ Pet.java
@@ -24,9 +24,10 @@ public class Pet {
         // Create a Pet object
         Pet pet1= new Pet("Buddy", "Dog");
         Pet maxPet = new Pet("Gary", "Snail");
-
+        Pet AhmedPet = new Pet("Cat," "Dogs");
+        
         System.out.println("my pets name is + " + maxPet.getName());
-   
+        System.out.ptint ("Ahmed favorite pets are + " +AhmedPet.getName());
         // Print the name of the pet
         System.out.println("Pet's name is: " + pet1.getName());
     }
